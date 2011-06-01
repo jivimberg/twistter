5f748f08265cb31fb2cc31b1a2b717ea52c0bd36
LucasApa.sec <LucasApa.sec@7943de8e-58e4-5a06-393d-d792ebb5192b>
2011-06-01 18:36:29 +0000

    TimelineService added, we need to consider the timeline pulling a service.

---
+++ src/twistter/android/client/TimelineService.java
@@ -0,0 +1,97 @@
+package twistter.android.client;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.TextView;
+
+public class TimelineService extends Service {
+	public static Activity ACTIVIDAD;
+	private Timer timer = null;
+
+	public static void establecerActividadPrincipal(Activity actividad){
+		TimelineService.ACTIVIDAD=actividad;
+	}
+
+	public void onCreate(){
+		super.onCreate();
+
+		// Iniciamos el servicio
+		this.iniciarServicio();
+
+		Log.i(getClass().getSimpleName(), "Servicio iniciado");
+	}
+
+	public void onDestroy()
+	{
+		super.onDestroy();
+
+		// Detenemos el serviciop
+		this.finalizarServicio();
+
+		Log.i(getClass().getSimpleName(), "Servicio detenido");
+	}
+
+	public IBinder onBind(Intent intent){
+		return null;
+	}
+
+	public void iniciarServicio(){
+		try{
+			Log.i(getClass().getSimpleName(), "Iniciando servicio...");
+			this.timer=new Timer();
+
+			// Configuramos lo que tiene que hacer
+			this.timer.scheduleAtFixedRate(
+				new TimerTask(){
+					public void run(){
+						ejecutarTarea();
+					}
+				},
+				0,
+				1000	// Cada 1 segundo se repite
+			);
+
+			Log.i(getClass().getSimpleName(), "Temporizador de timeline iniciado");
+		}
+		catch(Exception e){
+			Log.i(getClass().getSimpleName(), e.getMessage());
+		}
+	}
+
+	public void finalizarServicio(){
+		try{
+			Log.i(getClass().getSimpleName(), "Finalizando servicio...");
+
+			// Detenemos el timer
+			this.timer.cancel();
+
+			Log.i(getClass().getSimpleName(), "Temporizador de timeline detenido");
+
+		}
+		catch(Exception e){
+			Log.i(getClass().getSimpleName(), e.getMessage());
+		}
+	}
+
+	private void ejecutarTarea(){
+		Log.i(getClass().getSimpleName(), "Trayendo timeline...");
+
+		// Reflejamos la tarea en la actividad principal
+		TimelineService.ACTIVIDAD.runOnUiThread(new Runnable(){
+				public void run(){
+					//TextView ejecuciones=(TextView)TimelineService.ACTIVIDAD.findViewById(R.id.TextView01);
+					//ejecuciones.append(".");
+					//Aca podriamos poner algun loguito que refleje la conectividad
+				}
+			}
+		);
+	}
+}
+
