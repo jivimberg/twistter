a8cda6dbae81f76f695fda6d13ca850e9b254a30
jivimberg <jivimberg@7943de8e-58e4-5a06-393d-d792ebb5192b>
2011-06-11 12:19:15 +0000

    Improved TwitterService

--- server/src/services/TwitterService.java
+++ server/src/services/TwitterService.java
@@ -1,16 +1,18 @@
 package services;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import daos.AccessTokenDAO;
-import daos.impl.FileAccessTokenDAO;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
+import twitter4j.internal.org.json.JSONException;
+import utils.JSONUtils;
+import daos.AccessTokenDAO;
+import daos.impl.FileAccessTokenDAO;
 
 public class TwitterService {
 	
@@ -28,7 +30,7 @@ public class TwitterService {
 		return requestToken;
 	}
 	
-	//The request token passed here should be the same that was used to get the authorization URL
+	//The request token passed here must be the one used to get the authorization URL
 	public void persistAccessToken(String userId, String requestToken, String pin) throws TwitterException{
 		AccessToken accessToken = myTwitter.getOAuthAccessToken(requestToken, pin);
 		myTwitter.setOAuthAccessToken(accessToken);		
@@ -42,6 +44,17 @@ public class TwitterService {
 
 	public List<Status> getHomeTimeline() throws TwitterException{
 		return myTwitter.getHomeTimeline();
+	}
+	
+	public List<String> getJSONHomeTimeline() throws TwitterException, JSONException{
+		List<Status> timeline = myTwitter.getHomeTimeline();
+		List<String> jsonTimeline = new ArrayList<String>();
+		
+		for (Status status : timeline) {
+			jsonTimeline.add(JSONUtils.writeStatusToJSON(status));
+		}
+		
+		return jsonTimeline;
 	}
 	
 	public void updateStatus(String msg) throws TwitterException{
