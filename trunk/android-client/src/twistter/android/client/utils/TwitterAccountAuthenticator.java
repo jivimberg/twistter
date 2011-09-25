bf1dccc39c5732e08b6143a3a0b92666b2fa4a7c
kyronte@gmail.com <kyronte@gmail.com@7943de8e-58e4-5a06-393d-d792ebb5192b>
2011-09-25 23:57:38 +0000

    Agregando la logica para hacer Sync

---
+++ trunk/android-client/src/twistter/android/client/utils/TwitterAccountAuthenticator.java
@@ -0,0 +1,68 @@
+package twistter.android.client.utils;
+
+import android.accounts.AbstractAccountAuthenticator;
+import android.accounts.Account;
+import android.accounts.AccountAuthenticatorResponse;
+import android.accounts.NetworkErrorException;
+import android.content.Context;
+import android.os.Bundle;
+
+public class TwitterAccountAuthenticator extends AbstractAccountAuthenticator {
+
+	public TwitterAccountAuthenticator(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Bundle addAccount(AccountAuthenticatorResponse arg0, String arg1,
+			String arg2, String[] arg3, Bundle arg4)
+			throws NetworkErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bundle confirmCredentials(AccountAuthenticatorResponse response,
+			Account account, Bundle options) throws NetworkErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bundle editProperties(AccountAuthenticatorResponse response,
+			String accountType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bundle getAuthToken(AccountAuthenticatorResponse response,
+			Account account, String authTokenType, Bundle options)
+			throws NetworkErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAuthTokenLabel(String authTokenType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bundle hasFeatures(AccountAuthenticatorResponse response,
+			Account account, String[] features) throws NetworkErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bundle updateCredentials(AccountAuthenticatorResponse response,
+			Account account, String authTokenType, Bundle options)
+			throws NetworkErrorException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
