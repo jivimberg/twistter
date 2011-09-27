8a631dbc774edac1afef978fe8e02c51500ffd31
jivimberg <jivimberg@7943de8e-58e4-5a06-393d-d792ebb5192b>
2011-09-27 02:27:34 +0000

    Added char count on UpdateStatusActivity

--- trunk/android-client/src/twistter/android/client/activities/UpdateStatusActivity.java
+++ trunk/android-client/src/twistter/android/client/activities/UpdateStatusActivity.java
@@ -6,9 +6,12 @@ import twistter.android.client.ws.interfaces.UpdateStatusWebService;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class UpdateStatusActivity extends Activity {
@@ -23,12 +26,22 @@ public class UpdateStatusActivity extends Activity {
     	+ getString(R.string.ServerPort) + "/" + getString(R.string.ServerRootName) + "/" + getString(R.string.UpdateStatusWebService);
         
         Button tweetButton = (Button) findViewById(R.id.tweet_button);
+        
+        final EditText statusText = (EditText) findViewById(R.id.statusText);
+        final TextView textView = (TextView)findViewById(R.id.counter);
+        statusText.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                int charCount = statusText.getText().toString().length();
+                textView.setText(String.valueOf(140 - charCount) + " characters left");
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        }); 
 
         tweetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             	String response = null;
             	try {
-            		final EditText statusText = (EditText) findViewById(R.id.statusText);
             		final String msg = statusText.getText().toString();
             		final String username = getSharedPreferences(getString(R.string.PrefsName), MODE_PRIVATE).getString(getString(R.string.PrefUserName), null);
             		
