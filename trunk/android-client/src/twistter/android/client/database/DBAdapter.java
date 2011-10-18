17f409c698bbad6d5e4a4c9f42081bfbe3074aac
jivimberg <jivimberg@7943de8e-58e4-5a06-393d-d792ebb5192b>
2011-10-18 22:26:25 +0000

    Database up and running

--- trunk/android-client/src/twistter/android/client/database/DBAdapter.java
+++ trunk/android-client/src/twistter/android/client/database/DBAdapter.java
@@ -8,23 +8,22 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 public class DBAdapter {
-
-	public static final String KEY_ROWID = "_id";
-    public static final String KEY_TEXT ="jasonText";
-    private static final String TAG = "DBAdapter";
     
-    private static final String DATABASE_NAME = "Tweets";
-    private static final String DATABASE_TABLE = "jason";
+    private static final String DATABASE_NAME = "TwistterDB";
+    private static final String TWEETS_TABLE_NAME = "tweets";
     private static final int DATABASE_VERSION = 1;	
 	
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_JSON ="json";
+    public static final String KEY_USERNAME ="username";
 
     private static final String DATABASE_CREATE =
-        "create table titles (_id integer primary key autoincrement, "
+        "create table " + TWEETS_TABLE_NAME + " (_id integer primary key autoincrement, "
         + "username text not null, " 
-        + "tweetText text not null);";
+        + "json text not null);";
     
     
- private final Context context; 
+    private final Context context; 
     
     private DatabaseHelper DBHelper;
     private SQLiteDatabase db;
@@ -35,32 +34,6 @@ public class DBAdapter {
         DBHelper = new DatabaseHelper(context);
     }
         
-    private static class DatabaseHelper extends SQLiteOpenHelper 
-    {
-        DatabaseHelper(Context context) 
-        {
-            super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        }
-
-        @Override
-        public void onCreate(SQLiteDatabase db) 
-        {
-            db.execSQL(DATABASE_CREATE);
-        }
-
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, 
-                              int newVersion) 
-        {
-            Log.w(TAG, "Upgrading database from version " + oldVersion 
-                  + " to "
-                  + newVersion + ", which will destroy all old data");
-            db.execSQL("DROP TABLE IF EXISTS titles");
-            onCreate(db);
-        }
-    }    
-    
-
     //---opens the database---
      public DBAdapter open() throws SQLException 
      {
@@ -74,37 +47,77 @@ public class DBAdapter {
          DBHelper.close();
      }
      
-     //---insert a title into the database---
-     public long insertTweet(String ussername, String text,String image) 
+     //---insert a tweet---
+     public long insertTweet(String username, String json) 
      {
-         ContentValues initialValues = new ContentValues();
-         initialValues.put(KEY_TEXT, text);
-         return db.insert(DATABASE_TABLE, null, initialValues);
+         ContentValues values = createContentValues(username, json);
+         return db.insert(TWEETS_TABLE_NAME, null, values);
      }
 
+     //---updates a tweet---
+     public boolean updateTweet(long rowId, String username, String json) 
+     {
+         ContentValues newValues = createContentValues(username, json);
+         return db.update(TWEETS_TABLE_NAME, newValues, 
+                          KEY_ROWID + "=" + rowId, null) > 0;
+     }
+     
+     //---deletes tweet---
+     public boolean deleteTodo(long rowId) {
+ 		return db.delete(TWEETS_TABLE_NAME, KEY_ROWID + "=" + rowId, null) > 0;
+ 	}
   
      //---retrieves all the titles---
      public Cursor getAllTweets() 
      {
-         return db.query(DATABASE_TABLE, new String[] {
-         		KEY_ROWID, 
-         		KEY_TEXT},
-                 null, 
-                 null, 
-                 null, 
-                 null, null);
+		return db.query(TWEETS_TABLE_NAME,
+				new String[] { KEY_ROWID, KEY_JSON }, null, null, null, null, null);
      }
+     
+     //---retrieves a single tweet---
+     public Cursor fetchTweet(long rowId) throws SQLException {
+ 		Cursor mCursor = db.query(true, TWEETS_TABLE_NAME, new String[] {
+ 				KEY_ROWID, KEY_USERNAME, KEY_JSON},
+ 				KEY_ROWID + "=" + rowId, null, null, null, null, null);
+ 		if (mCursor != null) {
+ 			mCursor.moveToFirst();
+ 		}
+ 		return mCursor;
+ 	}
 
-    
-     //---updates a title---
-     public boolean updateTweet(long rowId, String ussername, 
-     String text, String image) 
+     private ContentValues createContentValues(String username, String json) 
      {
-         ContentValues args = new ContentValues();
-         args.put(KEY_TEXT, text);
-         return db.update(DATABASE_TABLE, args, 
-                          KEY_ROWID + "=" + rowId, null) > 0;
-     }
+ 		ContentValues values = new ContentValues();
+ 		values.put(KEY_USERNAME, username);
+ 		values.put(KEY_JSON, json);
+ 		return values;
+ 	}
+     
+     
+     private static class DatabaseHelper extends SQLiteOpenHelper 
+     {
+         DatabaseHelper(Context context) 
+         {
+             super(context, DATABASE_NAME, null, DATABASE_VERSION);
+         }
+
+         @Override
+         public void onCreate(SQLiteDatabase db) 
+         {
+             db.execSQL(DATABASE_CREATE);
+         }
+
+         @Override
+         public void onUpgrade(SQLiteDatabase db, int oldVersion, 
+                               int newVersion) 
+         {
+             Log.w("DatabaseAdapter", "Upgrading database from version " + oldVersion 
+                   + " to "
+                   + newVersion + ", which will destroy all old data");
+             db.execSQL("DROP TABLE IF EXISTS titles");
+             onCreate(db);
+         }
+     }  
     
     
 }
